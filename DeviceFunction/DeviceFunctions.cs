aea1668229c3d23ad2669f41bcc9f53fedc4cff4
Balázs Keresztúri <19balazs86@gmail.com>
2020-02-22 19:51:51 +0000

    Add functions to handle messages

--- DeviceFunction/DeviceFunctions.cs
+++ DeviceFunction/DeviceFunctions.cs
@@ -1,16 +1,35 @@
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
 
-namespace DeviceOfflineDetectionFunction
+namespace DeviceFunction
 {
   public static class DeviceFunctions
   {
     [FunctionName(nameof(HandleMessages))]
-    public static void HandleMessages(
-      [QueueTrigger("device-messages")] string deviceNumber,
+    public static async Task HandleMessages(
+      [QueueTrigger("device-messages")] string deviceId,
+      [DurableClient] IDurableEntityClient entityClient,
       ILogger log)
     {
-      log.LogInformation($"Queue trigger function processed: {deviceNumber}");
+      log.LogInformation($"{nameof(HandleMessages)} function processing: {deviceId}");
+
+      await entityClient.SignalEntityAsync<IDeviceEntity>(getEntityId(deviceId), device => device.MessageReceived());
     }
+
+    [FunctionName(nameof(HandleOfflineMessages))]
+    public static async Task HandleOfflineMessages(
+      [DurableClient] IDurableEntityClient entityClient,
+      [QueueTrigger(DeviceEntity.TimeOutQueue)] string deviceId,
+      ILogger log)
+    {
+      log.LogInformation($"{nameof(HandleOfflineMessages)} function processing: {deviceId}");
+
+      await entityClient.SignalEntityAsync<IDeviceEntity>(getEntityId(deviceId), device => device.DeviceTimeout());
+    }
+
+    private static EntityId getEntityId(string deviceId)
+      => new EntityId(nameof(DeviceEntity), deviceId);
   }
 }
