fe3ff13ec91a266238b8e3d3ce4dd1f8e5bb2ef2
Balázs Keresztúri <19balazs86@gmail.com>
2020-02-22 16:59:44 +0000

    Finish console application (test devices).

--- TestDeviceConsoleApp/Program.cs
+++ TestDeviceConsoleApp/Program.cs
@@ -1,13 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
 
 namespace TestDeviceConsoleApp
 {
   public class Program
   {
+    private static CloudQueue _queue;
+    private static Random _random = new Random();
+
     public static async Task Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      _queue = await createCloudQueue();
+
+      CancellationTokenSource cts = null;
+      Task messageSenderTask      = null;
+
+      while (true)
+      {
+        Console.WriteLine("Enter number of devices. 0 to exit");
+
+        string input     = Console.ReadLine();
+        int devicesCount = int.Parse(input);
+
+        cts?.Cancel();
+        messageSenderTask?.Wait();
+
+        if (devicesCount <= 0) break;
+
+        cts = new CancellationTokenSource();
+
+        messageSenderTask = messageSender(devicesCount, cts.Token);
+      }
+    }
+
+    private static async Task messageSender(int devicesCount, CancellationToken ct)
+    {
+      while (!ct.IsCancellationRequested)
+      {
+        List<Task> tasks = Enumerable
+          .Range(1, devicesCount)
+          .Select(sendDeviceId)
+          .ToList();
+
+        tasks.Add(Task.Delay(TimeSpan.FromSeconds(10), ct));
+
+        try
+        {
+          await Task.WhenAll(tasks);
+
+          Console.WriteLine("Messages has been sent.");
+        }
+        catch (TaskCanceledException)
+        {
+          Console.WriteLine("MessageSender is cancelled.");
+        }
+      }
+    }
+
+    private static async Task sendDeviceId(int deviceId)
+    {
+      await Task.Delay(_random.Next(1, 500));
+
+      await _queue.AddMessageAsync(new CloudQueueMessage(deviceId.ToString()));
+    }
+
+    private static async Task<CloudQueue> createCloudQueue()
+    {
+      //string storageConnString = Environment.GetEnvironmentVariable("CUSTOMCONNSTR_DeviceStorageConnString");
+      string storageConnString = "UseDevelopmentStorage=true";
+
+      CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnString);
+      CloudQueueClient queueClient       = storageAccount.CreateCloudQueueClient();
+
+      CloudQueue queue = queueClient.GetQueueReference("device-messages");
+
+      await queue.CreateIfNotExistsAsync();
+
+      return queue;
     }
   }
 }
