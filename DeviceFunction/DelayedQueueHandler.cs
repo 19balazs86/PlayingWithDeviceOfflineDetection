7f47873c640b924cc7d3defb852d747299a6c1a1
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-01 15:43:37 +0200

    Add: DelayedQueueHandler

---
+++ DeviceFunction/DelayedQueueHandler.cs
@@ -0,0 +1,74 @@
+﻿using Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace DeviceFunction;
+
+public interface IDelayedQueueHandler
+{
+    Task<(string messageId, string popReceipt)> SendMessage(string message);
+
+    Task<string?> UpdateMessage(string? messageId, string? popReceipt);
+
+    Task DeleteMessage(string? messageId, string? popReceipt);
+}
+
+public sealed class DelayedQueueHandler : IDelayedQueueHandler
+{
+    public const string TimeoutQueue = "timeout-messages";
+    public const string DeleteQueue  = "delete-devices";
+
+    private readonly QueueClient _queueClient;
+    private readonly TimeSpan _visibilityTimeout;
+
+    public DelayedQueueHandler(QueueClient queueClient, TimeSpan visibilityTimeout)
+    {
+        _queueClient       = queueClient;
+        _visibilityTimeout = visibilityTimeout;
+    }
+
+    public async Task<(string messageId, string popReceipt)> SendMessage(string message)
+    {
+        SendReceipt response = await _queueClient.SendMessageAsync(message, _visibilityTimeout);
+
+        return (response.MessageId, response.PopReceipt);
+    }
+
+    public async Task<string?> UpdateMessage(string? messageId, string? popReceipt)
+    {
+        if (string.IsNullOrWhiteSpace(messageId) || string.IsNullOrWhiteSpace(popReceipt))
+        {
+            return null;
+        }
+
+        try
+        {
+            UpdateReceipt response = await _queueClient.UpdateMessageAsync(messageId, popReceipt, visibilityTimeout: _visibilityTimeout);
+
+            return response.PopReceipt;
+        }
+        catch (RequestFailedException)
+        {
+            // There was a message but not any more. Add timeout message.
+
+            return null;
+        }
+    }
+
+    public async Task DeleteMessage(string? messageId, string? popReceipt)
+    {
+        if (string.IsNullOrWhiteSpace(messageId) || string.IsNullOrWhiteSpace(popReceipt))
+        {
+            return;
+        }
+
+        try
+        {
+            await _queueClient.DeleteMessageAsync(messageId, popReceipt);
+        }
+        catch (RequestFailedException)
+        {
+            // There was a message but not any more.
+        }
+    }
+}
--- DeviceFunction/Program.cs
+++ DeviceFunction/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Storage.Queues;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -5,6 +7,9 @@ namespace DeviceFunction;
 
 public static class Program
 {
+    public const string TimeoutQueueHandlerKey = "timeoutQueueHandler";
+    public const string DeleteQueueHandlerKey  = "deleteQueueHandler";
+
     public static void Main(string[] args)
     {
         IHost host = new HostBuilder()
@@ -17,6 +22,35 @@ public static class Program
 
     private static void configureServices(HostBuilderContext builderContext, IServiceCollection services)
     {
+        string storageConnString = builderContext.Configuration["AzureWebJobsStorage"]
+            ?? throw new NullReferenceException("Missing configration value for AzureWebJobsStorage");
 
+        // Azure .NET SDKs
+        // https://azure.github.io/azure-sdk/releases/latest/dotnet.html
+
+        // Example: https://kaylumah.nl/2022/02/21/working-with-azure-sdk-for-dotnet.html
+        // For DI container using the package: Microsoft.Extensions.Azure
+
+        services.AddAzureClients(clients =>
+        {
+            // Messages need to be encoded as Base64 because the QueueTrigger functions that way
+            clients.AddQueueServiceClient(storageConnString)
+                .ConfigureOptions(options => options.MessageEncoding = QueueMessageEncoding.Base64);
+        });
+
+        services.addDelayedQueueHandler(DelayedQueueHandler.TimeoutQueue, TimeoutQueueHandlerKey, TimeSpan.FromSeconds(30));
+        services.addDelayedQueueHandler(DelayedQueueHandler.DeleteQueue,  DeleteQueueHandlerKey,  TimeSpan.FromSeconds(60));
+    }
+
+    private static void addDelayedQueueHandler(this IServiceCollection services, string queueName, string serviceKey, TimeSpan visibilityTimeout)
+    {
+        services.AddKeyedScoped<IDelayedQueueHandler>(serviceKey, (sp, _) =>
+        {
+            QueueServiceClient queueServiceClient = sp.GetRequiredService<QueueServiceClient>();
+
+            QueueClient queueClient = queueServiceClient.GetQueueClient(queueName);
+
+            return new DelayedQueueHandler(queueClient, visibilityTimeout);
+        });
     }
 }
